12fd33b9b1354ec42d740794f132a1450675cf1d
csteffe <colin.steffe@unil.ch>
2021-11-05 20:03:56 +0100

    Footprint dataset Created a data folder with the two datasets Created a R script named read_data.R for the project's proposal reviwer to load the data easily

---
+++ read_data.R
@@ -0,0 +1,6 @@
+library(readr)
+
+options(scipen=999)
+
+NFA_2019_public_data <- read_csv("data/NFA 2019 public_data.csv")
+GFN_Country_Code_Concordance_Table <- read_csv("data/GFN Country Code Concordance Table.csv")
