1aebaed1363ce3721b0473bdcbaabc6703e2a4c1
JasmineBM <jasmine.mawjee@unil.ch>
2021-12-01 17:21:39 +0100

    Project.Rd creation
    Create the .Rmd file for the final project

--- read_data.R
+++
@@ -1,6 +0,0 @@
-library(readr)
-
-options(scipen=999)
-
-NFA_2019_public_data <- read_csv("data/NFA 2019 public_data.csv")
-GFN_Country_Code_Concordance_Table <- read_csv("data/GFN Country Code Concordance Table.csv")
