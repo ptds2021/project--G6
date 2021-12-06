0413d66ea9ccbd57c9f0849f50b71120cbbb028a
JasmineBM <jasmine.mawjee@unil.ch>
2021-12-06 10:44:27 +0100

    Add package library to server.R

--- G6/inst/shiny-examples/Sustshinability/server.R
+++ G6/inst/shiny-examples/Sustshinability/server.R
@@ -1,11 +1,12 @@
 library(shiny)
+library(G6)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-   
-    # Generate a plot to show the sustainability of the chosen country 
+
+    # Generate a plot to show the sustainability of the chosen country
     output$`Sustainibility plot` <- renderPlot({
       sustainability(input$country)
   })
-  
+
 })
