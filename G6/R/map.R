1629a003ee77fbe49c9421fc1967f34b892352b6
Yue2901 <80172395+Yue2901@users.noreply.github.com>
2021-12-06 12:30:11 +0100

    Function description
    Created worldmap function and added description

--- G6/R/map.R
+++ G6/R/map.R
@@ -1,48 +1,41 @@
-library(ggplot2)
-library(tidyverse)
-library(maps)
-library(dplyr)
-library(ggiraph)
-library(sp)
-library(sf)
-library(plotly)
-library(gganimate)
-library(transformr)
-library(readr)
-
-# ------------------------------------------------------
-
-data <- read_csv(here::here("NFA 2019 public_data.csv"))
+#' @title World Map
+#' @description Plot a world map displaying countries ecological footprintS.
+#' @return A World Map with the countries ecological footprint.
+#' @export
+worldmap <- function() {
+  data <- read_csv(here::here("NFA 2019 public_data.csv"))
 
 # Create a data frame of map data
-mapdata <- map_data("world") %>%
+mapdata <- ggplot2::map_data("world") %>%
            rename(country = region)
 
 country_chosen <- data %>%
-                  select(country, crop_land, year, record) %>% # shiny app: user input can choose the second parameter
-                  filter(country == "Switzerland" & record == "EFProdTotGHA") # shiny app: user input
+                  dplyr::select(country, crop_land, year, record) %>% # shiny app: user input can choose the second parameter
+                  dplyr::filter(country == "Switzerland" & record == "EFProdTotGHA") # shiny app: user input
 
-country_chosen <- left_join(mapdata,
+country_chosen <- dplyr::left_join(mapdata,
                             country_chosen,
                             by = "country")
 
-country_chosen2 <- country_chosen %>% filter(country == "Switzerland")
+country_chosen2 <- country_chosen %>% dplyr::filter(country == "Switzerland")
 
-map2 <- ggplot(data = country_chosen2,
-               mapping = aes(x = long,
+map2 <- ggplot2::ggplot(data = country_chosen2,
+               mapping = ggplot2::aes(x = long,
                              y = lat,
                              group = group)) +
 
-        geom_polygon(aes(fill = crop_land))  # shiny app: user input
+        ggplot2::geom_polygon(ggplot2::aes(fill = crop_land))  # shiny app: user input
 
 map_anim <- map2 +
-            transition_time(country_chosen2$year) +
-            ggtitle('Year: {frame_time}',
+            gganimate::transition_time(country_chosen2$year) +
+            ggplot2::ggtitle('Year: {frame_time}',
                     subtitle = 'Frame {frame} of {nframes}')
 
 num_years <- max(country_chosen2$year) - min(country_chosen2$year) + 1
 
-animate(map_anim,
+gganimate::animate(map_anim,
         nframes = num_years)
+
+}
 
 
