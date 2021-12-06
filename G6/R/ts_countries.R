370e2a73808555d4a7797ea44052945c95a81e7a
Yue2901 <80172395+Yue2901@users.noreply.github.com>
2021-12-06 12:11:21 +0100

    Function description
    Added the description for the carb_countries_ts function

--- G6/R/ts_countries.R
+++ G6/R/ts_countries.R
@@ -1,33 +1,37 @@
-library(dplyr)
-library(tidyverse)
-library(tsibble)
-library(fpp3)
-library(reshape2)
-library(readr)
-
+#' @title Time Series Plot
+#' @description Plot Times Series of countries ecological footprints, to observe its evolution over time.
+#' @param countries_list List of countries chosen by the user.
+#' @param record_type Record chosen by the user.
+#' @param doforecast Forecast.
+#' @param yearforecast Number of year for which do the forecast.
+#' @return A Time Series plot with the chosen countries ecological footprint.
+#' @export
+#' @examples
+#' country_list <- list("Spain", "Italy","France", "Germany","Switzerland")
+#' carb_countries_ts(country_list, "EFProdTotGHA", quo(fishing_ground), "No", 10)
 carb_countries_ts <- function(countries_list, record_type, indicator, doforecast, yearforecast){
 
   #read data
-  data <- read_csv(here::here("NFA 2019 public_data.csv"))
+  data <- readr::read_csv(here::here("NFA 2019 public_data.csv"))
 
 
   # select variables and filter for country and record
   data_country_ts <- data %>%
-                     select(year, country, record , !!indicator) %>%
-                     filter(country %in% country_list & record == record_type)
+                     dplyr::select(year, country, record , !!indicator) %>%
+                     dplyr::filter(country %in% country_list & record == record_type)
 
 
   data_country_ts <- data_country_ts %>%
-                     tsibble(index = year, key = country) %>%
-                     mutate(index=as.double(!!indicator))
+                     tsibble::tsibble(index = year, key = country) %>%
+                     dplyr::mutate(index=as.double(!!indicator))
 
   if(doforecast == "No"){
     #no forecast
     standard_plot <- data_country_ts %>%
                      autoplot(index) +
-                     ggtitle("Time series by country and year") +
-                     xlab("year") +
-                     ylab(paste0("for record : ", data_country_ts$record[1]))
+                     ggplot2::ggtitle("Time series by country and year") +
+                     ggplot2::xlab("year") +
+                     ggplot2::ylab(paste0("for record : ", data_country_ts$record[1]))
 
     return(standard_plot)
   }
@@ -40,17 +44,13 @@ carb_countries_ts <- function(countries_list, record_type, indicator, doforecast
     forecast_plot < -fit %>%
                      forecast(h = yearforecast) %>%
                      autoplot(data_country_ts) +
-                     ggtitle("Time series by country and year")+xlab("year") +
-                     ylab(paste0("for record : ", data_country_ts$record[1]))
+                     ggplot2::ggtitle("Time series by country and year") +
+                     ggplot2::xlab("year") +
+                     ggplot2::ylab(paste0("for record : ", data_country_ts$record[1]))
 
     return(forecast_plot)
   }
 
 
 }
-
-country_list <- list("Spain", "Italy","France", "Germany","Switzerland")
-
-carb_countries_ts(country_list, "EFProdTotGHA", quo(fishing_ground), "No", 10)
-
 
