059564a13c69c244f09bd09b6b513eb5bbc2124d
JasmineBM <jasmine.mawjee@unil.ch>
2021-12-07 17:02:44 +0100

    Update shiny
    Add selectInput

--- G6/inst/shiny-examples/Sustshinability/ui.R
+++ G6/inst/shiny-examples/Sustshinability/ui.R
@@ -5,23 +5,49 @@ library(shiny)
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Carbcalc"),
+  titlePanel("Sustshinability"),
   
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     
     sidebarPanel(
-       
-       # Input: Selector for countriy information to plot 
-       selectInput("country", "Choose a country:",
-                   c(unique(data$country)),
-                   multiple = TRUE) # add here vector of country with names
-                   
-    ),
+      
+      # Input: Selector for country information to plot 
+      selectInput("country", "Choose a country:",
+                  c(unique(data$country)),
+                  multiple = TRUE)), # add here vector of country with names
+      
+      # Input: Selector for country information to plot 
+      selectInput("country", "Choose a country:",
+                  c(unique(data$country)),
+                  multiple = TRUE)), # add here vector of country with names
+    
+      selectInput("indicator", "Choose a category:",
+                  c("crop_land",
+                    "grazing_land",
+                    "forest_land",
+                    "fishing_ground",
+                    "built_up_land",
+                    "carbon",
+                    "total"),
+                  multiple = FALSE)),
+  
+    selectInput("record_chosen", "Choose a record:",
+                c("AreaPerCap",
+                  "AreaTotHA",
+                  "BiocapPerCap",
+                  "BiocapTotGHA",
+                  "EFConsPerCap",
+                  "EFConsTotGHA",
+                  "EFProdPerCap",
+                  "EFProdTotGHA"),
+                multiple = FALSE))
     
     # Show a plot of the generated distribution
     mainPanel(
-       plotOutput("Sustainibility plot")
-    )
+       plotOutput("Sustainibility plot"))
+   )
   )
-))
+ )
+)
+
