c989e99ae8bada3e445efca74f063cdb61a40c49
JasmineBM <jasmine.mawjee@icloud.com>
2021-12-10 13:32:48 +0100

    uitest.R
    Plot EF columns in a facetted plot by record

--- G6/inst/shiny-examples/Sustshinability/uitest.R
+++ G6/inst/shiny-examples/Sustshinability/uitest.R
@@ -9,12 +9,34 @@ library(ggplot2)
 
 data <- read_csv(here::here("NFA 2019 public_data.csv"))
 
-switzerland <- data %>% 
+# Print class of every column
+class <- sapply(data, class)
+
+# Convert character to numeric
+i <- c("crop_land", "grazing_land", "forest_land", "fishing_ground", "built_up_land", "carbon")
+data <- data %>% 
+  mutate_at(i, as.numeric) 
+
+# data %>% str() # verification
+
+# To be able to compare the values of the records we need to scale the data so that 
+# it is on a comparable scale (compare apples with apples and not bananas)
+data_scaled <- data %>% 
+               mutate_at(i, scale)
+
+# Create subset for a country 
+switzerland <- data_scaled %>% 
                filter(country == "Switzerland")
 
-ggplot(switzerland,
-       aes(x = year, y = crop_land, group = 1)) + 
-  geom_path() +
-  facet_grid(record + crop_land ~ carbon, margins = TRUE)
+# Plot faceted by record 
+crop_land <- ggplot(switzerland,
+                    aes(x = year, y = crop_land, group = 1)) + 
+                    geom_path() +
+                    facet_wrap(~record, 
+                               scales = "free_y", 
+                               ncol = 4)
+
+                               
+
 
 
