9d9aa0be3103987d6eedebba77b3c17ad13e53aa
JasmineBM <jasmine.mawjee@unil.ch>
2021-12-13 10:12:53 +0100

    Update map_overview.R
    Add documentation using roxygen2

--- G6/R/map_overview.R
+++ G6/R/map_overview.R
@@ -1,8 +1,12 @@
+#' @title Map by record type in 2016
+#' @description The function returns an interactive World map of available countries displaying the total ecological footprint (crop land, grazing land, forest land, fishing ground, built-up land and carbon) of different production sites, by selected record type. The user can move the mouse cursor on a country and see more detailed information on the different production sites' ecological footprint.
+#' @param record_type Record type selected by the user ("AreaPerCap", "AreaTotHA", "BiocapPerCap", "BiocapTotGHA", "EFConsPerCap", "EFConsTotGHA", "EFProdPerCap", "EFProdTotGHA")
+#' @return An interactive World map
+#' @export
+#' @examples
+#' map_overview("AreaPerCap")
 
 map_overview <- function(record_type){
-
-  library(tidyverse)
-  library(dplyr)
 
   data <- readr::read_csv(here::here("NFA 2019 public_data.csv"))
 
@@ -29,6 +33,7 @@ map_overview <- function(record_type){
                                                               "<br>Grazing land: ", grazing_land,
                                                               "<br>Forest land: ", forest_land,
                                                               "<br>Fishing ground: ", fishing_ground,
+                                                              "<br>Built-up land: ", built_up_land,
                                                               "<br>Total: ", total))) +
 
     ggplot2::geom_polygon(aes(fill = total)) +
