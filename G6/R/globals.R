cc5dd36b4d32aae2a0a60ca2cb82ab89ce87ef38
JasmineBM <jasmine.mawjee@unil.ch>
2021-12-18 19:40:07 +0100

    Use unizipped data

--- G6/R/globals.R
+++ G6/R/globals.R
@@ -3,4 +3,5 @@ utils::globalVariables(c("year", "country", "record", "total",
                          "sustainability", "index", "region",
                          "long", "lat", "group", "crop_land",
                          "grazing_land", "forest_land", "fishing_ground",
-                         "built_up_land", "total", "indicator_name", "runApp"))
+                         "built_up_land", "total", "indicator_name", "runApp",
+                         "shinyApp"))
