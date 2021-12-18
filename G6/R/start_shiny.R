31898b33b29afac34d04b2f3016fc1ead0d34a31
csteffe <colin.steffe@unil.ch>
2021-12-18 17:16:38 +0100

    new function to start the app from the package
    - new R script with a function that needs to be included in the package

---
+++ G6/R/start_shiny.R
@@ -0,0 +1,12 @@
+#' @title Start shiny app
+#' @description Function to start the shiny app once the package is loaded
+#' @return start the shiny app
+#' @export
+#' @examples
+#' run_shiny ()
+
+
+
+run_shiny <- function(){
+  runApp('inst/shiny-examples/Sustshinability')
+}
