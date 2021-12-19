fd4b66d3e05719ed24e4a70a5beec35b92c813ad
JasmineBM <jasmine.mawjee@unil.ch>
2021-12-19 09:13:55 +0100

    Deleting unecessary files

--- G6/tests/testthat/test_sustainbility.R
+++ G6/tests/testthat/test_sustainbility.R
@@ -2,6 +2,6 @@
 
 # Test whether the map is visible
 test_that("The outcome is a barplot displaying our sustainability measure", {
-  sust <- G6::sustainability(France)
+  sust <- G6::sustainability("France")
   expect_visible(sust)
 })
