5b32eec6bd94e33ca7d9cabf181d4afcd4d0accd
JasmineBM <jasmine.mawjee@unil.ch>
2021-12-19 08:54:02 +0100

    Add tests in testthat

---
+++ G6/tests/testthat/test_sustainbility.R
@@ -0,0 +1,7 @@
+# Test sustainability
+
+# Test whether the map is visible
+test_that("The outcome is a barplot displaying our sustainability measure", {
+  sust <- G6::sustainability(France)
+  expect_visible(sust)
+})
